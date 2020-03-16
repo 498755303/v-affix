1244aeff6b5d4a5d48d1f9dd2eda2cbbbfc504a7
huliang@xm9m.com <huliang@xm9m.com>
2020-03-16 17:01:42 +0800

    新增组件

---
+++ examples/App.vue
@@ -0,0 +1,29 @@
+<!--
+ * @Description: 
+ * @LastEditors: HL
+ * @LastEditTime: 2020-03-16 16:50:37
+ -->
+<template>
+  <div id="app">
+    <demo></demo>
+  </div>
+</template>
+
+<script>
+import demo from "./demo";
+export default {
+  name: "app",
+  components: { demo }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ examples/demo.vue
@@ -0,0 +1,45 @@
+<!--
+ * @Description: 
+ * @LastEditors: HL
+ * @LastEditTime: 2020-03-16 16:53:36
+ -->
+<template>
+  <div class="adesk-affix-demo">
+    <p class="test"
+       v-for="item in list"
+       :key="'p1_'+item">为了测试</p>
+    <Affix :offset-top="50">
+      <span class="demo-affix">固定在顶部50px</span>
+    </Affix>
+    <Affix :offset-bottom="50">
+      <span class="demo-affix">固定在底部50px</span>
+    </Affix>
+    <p class="test"
+       v-for="item in list"
+       :key="'p2_'+item">为了测试</p>
+  </div>
+</template>
+<script>
+import Affix from "../packages/affix/index";
+export default {
+  data() {
+    return {
+      list: [1, 2, 3, 4, 5, 6, 7, 8]
+    };
+  },
+
+  components: {
+    Affix
+  },
+
+};
+</script>
+<style scoped lang="less">
+.test {
+  line-height: 80px;
+}
+.demo-affix {
+  background-color: blue;
+}
+
+</style>
